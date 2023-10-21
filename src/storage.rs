85889fff4f894d2607fc66f2dcebceaed05a0e37
Checksum <me@checksum.space>
2023-10-21 14:11:51 -0500

    refactor: storage path and add exists method

--- src/storage.rs
+++ src/storage.rs
@@ -5,32 +5,43 @@ use tokio::{
 };
 
 pub struct Storage {
-    pub path: String,
+    pub base_path: String,
 }
 
 impl Storage {
     pub async fn new(path: impl Into<String>) -> Result<Self> {
-        let path = path.into();
+        let base_path = path.into();
 
-        tokio::fs::create_dir_all(&path).await?;
-        Ok(Storage { path })
+        // todo: more elegant way to do this?
+        tokio::fs::create_dir_all(format!("{}/uploads", &base_path)).await?;
+        Ok(Storage { base_path })
     }
 
     pub async fn save(&self, id: impl Into<String>, bytes: &[u8]) -> Result<()> {
-        let mut file = File::create(format!("{}/{}.lumen", self.path, id.into())).await?;
+        let mut file =
+            File::create(format!("{}/uploads/{}.lumen", self.base_path, id.into())).await?;
         file.write_all(bytes).await?;
         Ok(())
     }
 
     pub async fn load(&self, id: impl Into<String>) -> Result<Vec<u8>> {
-        let mut file = File::open(format!("{}/{}.lumen", self.path, id.into())).await?;
+        let mut file =
+            File::open(format!("{}/uploads/{}.lumen", self.base_path, id.into())).await?;
         let mut bytes = Vec::new();
         file.read_to_end(&mut bytes).await?;
         Ok(bytes)
     }
 
     pub async fn delete(&self, id: impl Into<String>) -> Result<()> {
-        tokio::fs::remove_file(format!("{}/{}.lumen", self.path, id.into())).await?;
+        tokio::fs::remove_file(format!("{}/uploads/{}.lumen", self.base_path, id.into())).await?;
         Ok(())
+    }
+
+    pub async fn exists(&self, id: impl Into<String>) -> Result<bool> {
+        Ok(
+            tokio::fs::metadata(format!("{}/uploads/{}.lumen", self.base_path, id.into()))
+                .await
+                .is_ok(),
+        )
     }
 }
